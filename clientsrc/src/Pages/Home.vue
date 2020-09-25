2a016a1e921b430e2575a8ca665c3a146580749f
JustinBarnard1 <jbarnard0031@gmail.com>
2020-09-25 17:05:53 -0600

    started client bug home page

--- clientsrc/src/Pages/Home.vue
+++ clientsrc/src/Pages/Home.vue
@@ -1,12 +1,42 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <h1>Welcome to Your Vue.js App</h1>
+  <div class="home container-fluid">
+    <h1>Bring On The Bugs!</h1>
+    <div class="row">
+      <div class="col-8 card my-1 bg-primary" v-for="bug in bugs" :key="bug.id">
+        <div class="card-body">
+          <span>{{ bug.title }}</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "home"
+  name: "home",
+  mounted() {
+    this.$store.dispatch("getBugs");
+  },
+  data() {
+    return {
+      newBug: {
+        title: "",
+        description: "",
+      },
+    };
+  },
+  computed: {
+    bugs() {
+      return this.$store.state.bugs;
+    },
+    profile() {
+      return this.$auth.userInfo;
+    },
+  },
+  addBug() {
+    this.$store.dispatch("addBug", this.newBug);
+    this.newBug = { title: "", description: "" };
+  },
+  editBug() {},
 };
 </script>
