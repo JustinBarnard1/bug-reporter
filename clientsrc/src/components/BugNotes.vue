167555c3c4834692fb3dbe1aa8e467e409e873f7
JustinBarnard1 <jbarnard0031@gmail.com>
2020-09-27 19:17:14 -0600

    styling tables complete

--- clientsrc/src/components/BugNotes.vue
+++ clientsrc/src/components/BugNotes.vue
@@ -1,8 +1,8 @@
 <template>
-  <tr>
-    <td class>{{ noteProp.creatorEmail }}</td>
-    <td>{{ noteProp.content }}</td>
-    <td><i class="fa fa-trash" aria-hidden="true"></i></td>
+  <tr class="d-flex">
+    <td class="col-3">{{ noteProp.creatorEmail }}</td>
+    <td class="col-8">{{ noteProp.content }}</td>
+    <td class="col-2"><i class="fa fa-trash" aria-hidden="true"></i></td>
   </tr>
 </template>
 
